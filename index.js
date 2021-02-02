45c27aa10a0e70207a556148275a9b2f90ba7333
JacopoLuri <geomjacopoluri@gmail.com>
2021-02-02 12:35:35 +0100

    undated comments

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const puppeteer = require('puppeteer')
 
-// launch an instace of Chromiun, go to url then apply a document.queryselector
+// launch an instace of Chromiun, go to url then apply a document.querySelectorAll on the page
+
 async function scraper(url) {
+    // launch chromiun
     const browser = await puppeteer.launch();
+    // open a new page
     const page = await browser.newPage()
+    // opening selected url
     await page.goto(url);
     
-
-    
-
-
-    //  this thing work
+    // query here with $$eval
     const imgsUrl = await page.$$eval('img', imgs => imgs.map(img => img.getAttribute('src')));
     const imgsAlt = await page.$$eval('img', imgs => imgs.map(img => img.getAttribute('alt')));
  
-
+    // creating the onj for the FE
     const imgObj = imgsUrl.map((img, index) => ({imageSrc: img, imageAlt: imgsAlt[index]}))
 
-    console.log(imgObj)
-
+    // close chromiun
     await browser.close()
 }
 
+// test url
 scraper('https://www.sciencemag.org/news/2019/11/here-s-better-way-convert-dog-years-human-years-scientists-say')
